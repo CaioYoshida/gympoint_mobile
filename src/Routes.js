8e813c6d5280a8ddd8584b2ed03f3a94c6d77008
Caio Yoshida <caio.yoshida@yahoo.com.br>
2019-12-27 15:18:53 -0300

    chore: Styling Bottom Bar

--- src/Routes.js
+++ src/Routes.js
@@ -1,6 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
 import { createAppContainer, createSwitchNavigator } from 'react-navigation';
-import { createStackNavigator } from 'react-navigation-stack';
+import { createStackNavigator, HeaderBackButton } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import SignIn from '~/pages/SignIn';
 
@@ -11,29 +15,50 @@ import Help_order from '~/pages/Help_order';
 
 import Header from '~/Components/Header';
 
+function SubmitIconCheckins({ tintColor }) {
+  return <Icon name="arrow-upward" size={20} color={tintColor} />;
+}
+
+function SubmitIconHelp_order({ tintColor }) {
+  return <Icon name="assignment" size={20} color={tintColor} />;
+}
+
 export default createAppContainer(
   createSwitchNavigator(
     {
       SignIn,
       App: createBottomTabNavigator(
         {
-          Checkins,
-          Help_orders: createStackNavigator(
-            {
-              List,
-              Answer,
-              Help_order,
+          Checkins: {
+            screen: Checkins,
+            navigationOptions: {
+              tabBarLabel: 'Check-ins',
+              tabBarIcon: SubmitIconCheckins,
             },
-            {
-              initialRouteName: 'List',
-              defaultNavigationOptions: {
-                header: Header,
+          },
+          Help_orders: {
+            screen: createStackNavigator(
+              {
+                List,
+                Answer,
+                Help_order,
               },
-              cardStyle: {
-                backgroundColor: '#fff',
-              },
-            }
-          ),
+              {
+                initialRouteName: 'List',
+                defaultNavigationOptions: {
+                  header: Header,
+                  headerLeft: HeaderBackButton,
+                },
+                cardStyle: {
+                  backgroundColor: '#fff',
+                },
+              }
+            ),
+            navigationOptions: {
+              tabBarLabel: 'Pedir ajuda',
+              tabBarIcon: SubmitIconHelp_order,
+            },
+          },
         },
         {
           tabBarOptions: {
@@ -42,6 +67,8 @@ export default createAppContainer(
             inactiveTintColor: '#999',
             style: {
               backgroundColor: '#FFF',
+              height: 50,
+              marginBottom: 5,
             },
             initialRouteName: 'Checkins',
           },
@@ -53,3 +80,11 @@ export default createAppContainer(
     }
   )
 );
+
+SubmitIconCheckins.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
+
+SubmitIconHelp_order.propTypes = {
+  tintColor: PropTypes.string.isRequired,
+};
